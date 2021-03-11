bbfc56816b7a4dc8ae6ef8e532d726977c62f4ca
Junseuk <76210064+junseuk@users.noreply.github.com>
2021-03-11 22:54:45 +0900

    Update Q6_1.java
    update a deck instance and new methods in it

--- src/Q6_1.java
+++ src/Q6_1.java
@@ -1,12 +1,16 @@
 
 public class Q6_1 {
 	public static void main(String args[]) {
-		SutdaCard card1 = new SutdaCard(3, false);
-		SutdaCard card2 = new SutdaCard();
-		System.out.println(card1.info()); 
-		System.out.println(card2.info());
+		SutdaDeck deck = new SutdaDeck();
+		for(int i=0; i<deck.cards.length;i++) {
+			System.out.print(deck.cards[i]+",");
+		}
+		System.out.println();
+		System.out.println(deck.pick());
+		System.out.println(deck.pick(0));
+		deck.shuffle();
+		System.out.println(deck.pick(0));
 	}
-
 }
 
 class SutdaCard {
@@ -19,7 +23,41 @@ class SutdaCard {
 	}
 	int num;
 	boolean isKwang;
-	String info(){
+	public String toString() {
 		return num + (isKwang ? "K" : "");
 	}
+}
+
+class SutdaDeck {
+	final int CARD_NUM = 20;
+	SutdaCard[] cards = new SutdaCard[CARD_NUM];
+	SutdaDeck() {
+		for(int i=0;i<CARD_NUM;i++) {
+			int number = i+1;
+			boolean isKwang = false;
+			if (i+1 == 1|| i+1 ==3 || i+1 ==8) {
+				isKwang = true;
+			}
+			if (number>10) number = number - 10;
+			cards[i] = new SutdaCard(number, isKwang);
+		}
+	}
+	void shuffle() {
+		for(int i=0;i<CARD_NUM;i++) {
+			SutdaCard tmp = new SutdaCard();
+			int shuffleindex = (int) (Math.random() * CARD_NUM);
+			tmp = cards[i];
+			cards[i] = cards[shuffleindex];
+			cards[shuffleindex] = tmp;
+		}
+	}
+	SutdaCard pick(int index) {
+		if (index < 0 || index >= CARD_NUM) return null;
+		return cards[index];
+	}
+	SutdaCard pick() {
+		int randomindex = (int) (Math.random() * CARD_NUM);
+		return cards[randomindex];
+	}
+	
 }
